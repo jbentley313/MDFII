227cf4ca53568b53325e86dfef6c488aba0176df
Jason Bentley <jbxfade@fullsail.edu>
2013-07-08 17:52:41 -0700

    initial commit

---
+++ project1/project1/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  project1
+//
+//  Created by theOwner on 7/8/13.
+//  Copyright (c) 2013 Jason Bentley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ project1/project1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  project1
+//
+//  Created by theOwner on 7/8/13.
+//  Copyright (c) 2013 Jason Bentley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
