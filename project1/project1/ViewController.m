3339ca7242520f914d75bcca2751d61a3c488e2c
Jason Bentley <jbxfade@fullsail.edu>
2013-07-09 17:17:05 -0700

    tableview

--- project1/project1/ViewController.m
+++ project1/project1/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import <Accounts/Accounts.h>
+#import <Social/Social.h>
 
 @interface ViewController ()
 
@@ -16,6 +18,71 @@
 
 - (void)viewDidLoad
 {
+    //accountStore
+    ACAccountStore *accountStore = [[ACAccountStore alloc] init];
+    if (accountStore != nil) {
+        
+        //account type
+        ACAccountType *accountType = [accountStore accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+        if (accountType != nil) {
+            [accountStore requestAccessToAccountsWithType:accountType options:nil completion:^(BOOL granted, NSError *error) {
+                if (granted)
+                {
+                    //array of accounts
+                    NSArray *twitterAccounts = [accountStore accountsWithAccountType:accountType];
+                    if (twitterAccounts != nil) {
+                        
+                        //AcAcount
+                        ACAccount *currentAccount = [twitterAccounts objectAtIndex:0];
+                        if (currentAccount != nil) {
+                            
+                            //Rest call for user timeline
+                            NSString *userTimeString = @"https://api.twitter.com/1.1/statuses/user_timeline.json";
+                            
+                            //SLRequest
+                            SLRequest *slRequest = [SLRequest requestForServiceType:SLServiceTypeTwitter requestMethod:SLRequestMethodGET URL:[NSURL URLWithString:userTimeString] parameters:nil];
+                            
+                            if (slRequest != nil) {
+                                
+                                //1.1 Api requires a user to be logged in
+                                [slRequest setAccount:currentAccount];
+                                
+                                [slRequest performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error) {
+                                    
+                                    //get response code
+                                    NSInteger responseCode = [urlResponse statusCode];
+                                    
+                                    if (responseCode == 200) {
+                                    
+                                        //json object response
+                                        NSArray *twitterFeed = [NSJSONSerialization JSONObjectWithData:responseData options:0 error:nil];
+                                        
+                                        if (twitterFeed != nil) {
+                                            NSLog(@"%@", [twitterFeed description]);
+                                            
+                                            
+                                        }
+                                        
+                                    }
+                                    
+                                }];
+                            }
+                            
+                        }
+                        
+                        
+                    }
+                    
+                }
+                else
+                {
+                    
+                }
+            }];
+                                                                                               
+        }
+    }
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
