f583603c5d591be96309d54eb9d9e9e0e0c3cea0
Jason Bentley <jbxfade@fullsail.edu>
2013-07-30 09:42:54 -0700

    pics

--- project4/project4/ViewController.m
+++ project4/project4/ViewController.m
@@ -27,20 +27,20 @@
     
     //movie info
     MovieInfo *Movie1 = [[MovieInfo alloc] initWithMovieName:@"Touching the Void" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 6:00pm ", @" 7:00pm ", @" 8:20pm ",nil]];
-    MovieInfo *Movie2 = [[MovieInfo alloc] initWithMovieName:@"The Dictator" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 6:30pm ", @" 7:30pm ", @" 9:20pm ",nil]];
-    MovieInfo *Movie3 = [[MovieInfo alloc] initWithMovieName:@"Rock Star" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 5:00pm ", @" 6:00pm ", @" 7:40pm ",nil]];
-    MovieInfo *Movie4 = [[MovieInfo alloc] initWithMovieName:@"Studio Life" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 4:00pm ", @" 7:20pm ", @" 9:25pm ",nil]];
-    MovieInfo *Movie5 = [[MovieInfo alloc] initWithMovieName:@"Autotune Massacre" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 6:00pm ", @" 6:20pm ", @" 10:20pm ",nil]];
-    MovieInfo *Movie6 = [[MovieInfo alloc] initWithMovieName:@"You Can't Sing!" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 12:00pm ", @" 7:25pm ", @" 8:00pm ",nil]];
-    MovieInfo *Movie7 = [[MovieInfo alloc] initWithMovieName:@"Dancing With the Nerds" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 12:10pm ", @" 3:00pm ", @" 4:20pm ",nil]];
-    MovieInfo *Movie8 = [[MovieInfo alloc] initWithMovieName:@"Crazy Crazy Movie" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 12:15pm ", @" 1:00pm ", @" 8:10pm ",nil]];
-    MovieInfo *Movie9 = [[MovieInfo alloc] initWithMovieName:@"The Most Boring Movie" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 12:30pm ", @" 2:00pm ", @" 8:15pm ",nil]];
-    MovieInfo *Movie10 = [[MovieInfo alloc] initWithMovieName:@"Racecars!" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 11:00am ", @" 4:10pm ", @" 6:20pm ",nil]];
-    MovieInfo *Movie11 = [[MovieInfo alloc] initWithMovieName:@"Xcode is Better!" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 6:40pm ", @" 7:00pm ", @" 9:20pm ",nil]];
-    MovieInfo *Movie12 = [[MovieInfo alloc] initWithMovieName:@"Who Codes with Java?" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 6:45pm ", @" 7:05pm ", @" 9:35pm ",nil]];
-    MovieInfo *Movie13 = [[MovieInfo alloc] initWithMovieName:@"Console Dot Logging" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 6:50pm ", @" 7:35pm ", @" 8:20pm ",nil]];
-    MovieInfo *Movie14 = [[MovieInfo alloc] initWithMovieName:@"Java's Script" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 6:550pm ", @" 7:20pm ", @" 8:25pm ",nil]];
-    MovieInfo *Movie15 = [[MovieInfo alloc] initWithMovieName:@"We are Movie Fifteen" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 4:50pm ", @" 7:00pm ", @" 8:50pm ",nil]];
+    MovieInfo *Movie2 = [[MovieInfo alloc] initWithMovieName:@"The Avengers" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 6:30pm ", @" 7:30pm ", @" 9:20pm ",nil]];
+    MovieInfo *Movie3 = [[MovieInfo alloc] initWithMovieName:@"Star Trek" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 5:00pm ", @" 6:00pm ", @" 7:40pm ",nil]];
+    MovieInfo *Movie4 = [[MovieInfo alloc] initWithMovieName:@"Monster's University" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 4:00pm ", @" 7:20pm ", @" 9:25pm ",nil]];
+    MovieInfo *Movie5 = [[MovieInfo alloc] initWithMovieName:@"Arthur Newman" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 6:00pm ", @" 6:20pm ", @" 10:20pm ",nil]];
+    MovieInfo *Movie6 = [[MovieInfo alloc] initWithMovieName:@"The Hangover III" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 12:00pm ", @" 7:25pm ", @" 8:00pm ",nil]];
+    MovieInfo *Movie7 = [[MovieInfo alloc] initWithMovieName:@"Prometheus" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 12:10pm ", @" 3:00pm ", @" 4:20pm ",nil]];
+    MovieInfo *Movie8 = [[MovieInfo alloc] initWithMovieName:@"A Good Day to Die Hard" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 12:15pm ", @" 1:00pm ", @" 8:10pm ",nil]];
+    MovieInfo *Movie9 = [[MovieInfo alloc] initWithMovieName:@"The Lone Ranger" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 12:30pm ", @" 2:00pm ", @" 8:15pm ",nil]];
+    MovieInfo *Movie10 = [[MovieInfo alloc] initWithMovieName:@"World War Z" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 11:00am ", @" 4:10pm ", @" 6:20pm ",nil]];
+    MovieInfo *Movie11 = [[MovieInfo alloc] initWithMovieName:@"Pacific Rim" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 6:40pm ", @" 7:00pm ", @" 9:20pm ",nil]];
+    MovieInfo *Movie12 = [[MovieInfo alloc] initWithMovieName:@"The Wolverine" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 6:45pm ", @" 7:05pm ", @" 9:35pm ",nil]];
+    MovieInfo *Movie13 = [[MovieInfo alloc] initWithMovieName:@"Man of Steel" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 6:50pm ", @" 7:35pm ", @" 8:20pm ",nil]];
+    MovieInfo *Movie14 = [[MovieInfo alloc] initWithMovieName:@"Iron Man 3" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 6:550pm ", @" 7:20pm ", @" 8:25pm ",nil]];
+    MovieInfo *Movie15 = [[MovieInfo alloc] initWithMovieName:@"The Dark Knight Rises" mImage:@"img" mTimes:[NSArray arrayWithObjects:@" 4:50pm ", @" 7:00pm ", @" 8:50pm ",nil]];
     
     //array of movies
     movieArray = [[NSArray alloc] initWithObjects:Movie1, Movie2, Movie3, Movie4, Movie5, Movie6, Movie7, Movie8, Movie9, Movie10, Movie11, Movie12, Movie13, Movie14, Movie15, nil];
